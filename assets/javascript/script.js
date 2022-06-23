14d6eab054367c9f99b7eeb1e7c79ff3dc37e175
soulslurpee <charles.d.thompson87@gmail.com>
2022-06-23 16:57:13 -0400

    Added colorUpdate function. Functions correctly.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
 
   $("#currentDay").text(moment().format('DD MMMM YYYY, HH:MM:SS'));
+
+
+  var colorUpdate = function () {
+
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("h")[0]);
+      console.log( blockHour, currentHour)
+
+        if (blockHour < currentHour) {
+          $(this).removeClass("present").removeClass("future").addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).removeClass("past").removeClass("future").addClass("present");
+        } else {
+          $(this).removeClass("past").removeClass("present").addClass("future");
+        }
+      })
+  }
+  colorUpdate();
 });
+  
+  
